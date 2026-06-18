7775477629c9b9b0680a3c33f143f5b18e31f7cf
Robin L. Velte <robin.leandro.velte>
2026-06-18 18:51:37 +0200

    refactor: remove obsolete DotBahn.Shared and DotBahn.Modules projects

--- source/DotBahn.Modules/DotBahn.Modules.Authorization/NullAuthorization.cs
+++
@@ -1,9 +0,0 @@
-namespace DotBahn.Modules.Authorization;
-
-/// <summary>
-/// No-op implementation of the authorization system.
-/// </summary>
-public class NullAuthorization : IAuthorization {
-    /// <inheritdoc />
-    public void AuthorizeRequest(HttpRequestMessage request) { }
-}
