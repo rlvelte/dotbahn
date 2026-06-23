71c6519eaffb9f728a4244a5f01105ee4e5e30a6
Robin L. Velte <robin.leandro.velte>
2026-06-23 12:38:31 +0200

    feat: add area serach to facility query

--- source/DotBahn.Facilities/FacilityQuery.cs
+++ source/DotBahn.Facilities/FacilityQuery.cs
@@ -30,6 +30,38 @@ public sealed record FacilityQuery {
     public string? StationId { get; set; }
 
     /// <summary>
+    /// Westernmost longitude in WGS84 decimal degrees for geographic bounding box filter.
+    /// <remarks>
+    /// Must be used together with <see cref="LatitudeSouth"/>, <see cref="LongitudeEast"/>, and <see cref="LatitudeNorth"/>.
+    /// </remarks>
+    /// </summary>
+    public double? LongitudeWest { get; set; }
+
+    /// <summary>
+    /// Southernmost latitude in WGS84 decimal degrees for geographic bounding box filter.
+    /// <remarks>
+    /// Must be used together with <see cref="LongitudeWest"/>, <see cref="LongitudeEast"/>, and <see cref="LatitudeNorth"/>.
+    /// </remarks>
+    /// </summary>
+    public double? LatitudeSouth { get; set; }
+
+    /// <summary>
+    /// Easternmost longitude in WGS84 decimal degrees for geographic bounding box filter.
+    /// <remarks>
+    /// Must be used together with <see cref="LongitudeWest"/>, <see cref="LatitudeSouth"/>, and <see cref="LatitudeNorth"/>.
+    /// </remarks>
+    /// </summary>
+    public double? LongitudeEast { get; set; }
+
+    /// <summary>
+    /// Northernmost latitude in WGS84 decimal degrees for geographic bounding box filter.
+    /// <remarks>
+    /// Must be used together with <see cref="LongitudeWest"/>, <see cref="LatitudeSouth"/>, and <see cref="LongitudeEast"/>.
+    /// </remarks>
+    /// </summary>
+    public double? LatitudeNorth { get; set; }
+
+    /// <summary>
     /// Sets the facility type filter.
     /// </summary>
     public FacilityQuery WithType(FacilityType type) {
@@ -62,11 +94,33 @@ public sealed record FacilityQuery {
     }
 
     /// <summary>
+    /// Filters facilities within a geographic bounding box.
+    /// </summary>
+    /// <param name="lngWest">Westernmost longitude in WGS84 decimal degrees.</param>
+    /// <param name="latSouth">Southernmost latitude in WGS84 decimal degrees.</param>
+    /// <param name="lngEast">Easternmost longitude in WGS84 decimal degrees.</param>
+    /// <param name="latNorth">Northernmost latitude in WGS84 decimal degrees.</param>
+    /// <returns>The current <see cref="FacilityQuery"/> instance for fluent chaining.</returns>
+    public FacilityQuery WithArea(double lngWest, double latSouth, double lngEast, double latNorth) {
+        LongitudeWest = lngWest;
+        LatitudeSouth = latSouth;
+        LongitudeEast = lngEast;
+        LatitudeNorth = latNorth;
+        return this;
+    }
+
+    /// <summary>
     /// Converts the query into <see cref="QueryParameters"/> for API calls.
     /// </summary>
-    internal QueryParameters ToQueryParameters() => QueryParameters.Create()
-        .Add("type", EnumUtil.Format(Type, FacilitiesJsonContext.Default.FacilityType))
-        .Add("state", EnumUtil.Format(State, FacilitiesJsonContext.Default.FacilityState))
-        .Add("equipmentnumbers", EquipmentNumbers)
-        .Add("stationnumber", StationId);
+    internal QueryParameters ToQueryParameters() {
+        var area = LongitudeWest.HasValue && LatitudeSouth.HasValue && LongitudeEast.HasValue && LatitudeNorth.HasValue
+            ? FormattableString.Invariant($"{LongitudeWest},{LatitudeSouth},{LongitudeEast},{LatitudeNorth}") : null;
+
+        return QueryParameters.Create()
+            .Add("type", EnumUtil.Format(Type, FacilitiesJsonContext.Default.FacilityType))
+            .Add("state", EnumUtil.Format(State, FacilitiesJsonContext.Default.FacilityState))
+            .Add("equipmentnumbers", EquipmentNumbers)
+            .Add("stationnumber", StationId)
+            .Add("area", area);
+    }
 }
